7991223deabb64a358f7c0a5d6eab71ed23d3f2e
sophielevens <sophielevens1@gmail.com>
2021-03-24 18:49:42 +0000

    fixes auth issues

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,19 +1,14 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-
-import { Button } from "@material-ui/core";
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
-  return (
-    <Button
-      color="primary"
-      variant="contained"
-      onClick={() => loginWithRedirect()}
-    >
-      Log In
-    </Button>
-  );
-};
 
-export default LoginButton;
+  return (
+    <div>
+      <button onClick={() => loginWithRedirect}>sign in</button>
+    </div>
+  )
+}
+
+export default LoginButton
